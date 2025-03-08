31ad3976236c5fc6aa7fa40f289947a52ea549c3
vagrant <Nava200>
2025-03-08 15:10:34 +0000

    Added gorilla/mux dependency

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
+    "github.com/gorilla/mux" // External dependency for routing
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World! This is a Go web app.")
+    fmt.Fprintf(w, "Hello, World! This is a Go web app using Gorilla Mux.")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("Server is running on port 8000...")
-	http.ListenAndServe(":8000", nil)
+    r := mux.NewRouter()
+    r.HandleFunc("/", handler)
+    fmt.Println("Server is running on port 8000...")
+    http.ListenAndServe(":8000", r) // Use Gorilla Mux router
 }
